4c06f90cd582d47edcfb917af4a88069de5a4f41
Jerome Anciaux <jeskaa@hotmail.fr>
2024-11-12 23:32:23 +0100

    finished exo

---
+++ db/migrate/20241112133011_create_task.rb
@@ -0,0 +1,10 @@
+class CreateTask < ActiveRecord::Migration[7.1]
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.text :details
+      t.boolean :completed
+      t.timestamps
+    end
+  end
+end
